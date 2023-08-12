5b5b9c32bc6d8ca8015af7198d2b6dce2d68a681
Edward Lo <ed700a@gmail.com>
2023-08-12 15:30:39 +0800

    Add files via upload

---
+++ scanWifi/ver1/scanWifi.ino
@@ -0,0 +1,130 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+const char* ssid = "BITLAB GUEST";
+const char* password = "BITLAB123";
+const char* udpAddress = "192.168.1.48";
+const int udpPort = 8000; 
+int counter = 1;
+int delayTime = 700; // Minimum 700
+WiFiUDP udpClient;
+
+void setup() {
+    Serial.begin(115200);
+
+    Serial.printf("Connecting to %s\n", ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    Serial.print("UDP client IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.printf("Scanning WiFi networks, Number of trial: %d\n", counter);
+    int n = WiFi.scanNetworks();
+
+    String message = "";
+    for (int i = 0; i < n; i++) {
+        String ssid = WiFi.SSID(i);
+        int rssi = WiFi.RSSI(i);
+        int channel = WiFi.channel(i);
+        String encryption;
+        switch (WiFi.encryptionType(i)){
+            case WIFI_AUTH_OPEN:
+                encryption = "open";
+                break;
+            case WIFI_AUTH_WEP:
+                encryption = "WEP";
+                break;
+            case WIFI_AUTH_WPA_PSK:
+                encryption = "WPA";
+                break;
+            case WIFI_AUTH_WPA2_PSK:
+                encryption = "WPA2";
+                break;
+            case WIFI_AUTH_WPA_WPA2_PSK:
+                encryption = "WPA+WPA2";
+                break;
+            case WIFI_AUTH_WPA2_ENTERPRISE:
+                encryption = "WPA2-EAP";
+                break;
+            case WIFI_AUTH_WPA3_PSK:
+                encryption = "WPA3";
+                break;
+            case WIFI_AUTH_WPA2_WPA3_PSK:
+                encryption = "WPA2+WPA3";
+                break;
+            case WIFI_AUTH_WAPI_PSK:
+                encryption = "WAPI";
+                break;
+            default:
+                encryption = "unknown";
+        }
+
+        message += ssid + "," + String(rssi) + "," + String(channel) + "," + encryption + "\n";
+    }
+
+    udpClient.beginPacket(udpAddress, udpPort);
+    udpClient.print(message);
+    udpClient.endPacket();
+    Serial.println("Finished sending"); 
+    counter++;
+    //delay(delayTime);
+}
+
+void loop() {
+    Serial.printf("Scanning WiFi networks, Number of trial: %d\n", counter);
+    int n = WiFi.scanNetworks();
+
+    String message = "";
+    for (int i = 0; i < n; i++) {
+        String ssid = WiFi.SSID(i);
+        int rssi = WiFi.RSSI(i);
+        int channel = WiFi.channel(i);
+        String encryption;
+        switch (WiFi.encryptionType(i)){
+            case WIFI_AUTH_OPEN:
+                encryption = "open";
+                break;
+            case WIFI_AUTH_WEP:
+                encryption = "WEP";
+                break;
+            case WIFI_AUTH_WPA_PSK:
+                encryption = "WPA";
+                break;
+            case WIFI_AUTH_WPA2_PSK:
+                encryption = "WPA2";
+                break;
+            case WIFI_AUTH_WPA_WPA2_PSK:
+                encryption = "WPA+WPA2";
+                break;
+            case WIFI_AUTH_WPA2_ENTERPRISE:
+                encryption = "WPA2-EAP";
+                break;
+            case WIFI_AUTH_WPA3_PSK:
+                encryption = "WPA3";
+                break;
+            case WIFI_AUTH_WPA2_WPA3_PSK:
+                encryption = "WPA2+WPA3";
+                break;
+            case WIFI_AUTH_WAPI_PSK:
+                encryption = "WAPI";
+                break;
+            default:
+                encryption = "unknown";
+        }
+
+        message += ssid + "," + String(rssi) + "," + String(channel) + "," + encryption + "\n";
+    }
+
+    udpClient.beginPacket(udpAddress, udpPort);
+    udpClient.print(message);
+    udpClient.endPacket();
+    Serial.println("Finished sending"); 
+    counter++;
+    //delay(delayTime);
+}
---
+++ scanWifi/wifiDataReceiveServer.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+
+UDP_IP_ADDRESS = "192.168.1.48"
+UDP_PORT = 8000 
+
+def sort_wifi_networks(networks, sort_by):
+    if sort_by == 'ssid':
+        sorted_networks = sorted(networks, key=lambda x: x['ssid'])
+    elif sort_by == 'rssi':
+        sorted_networks = sorted(networks, key=lambda x: x['rssi'], reverse=True)
+    elif sort_by == 'ssid_rssi':
+        sorted_networks = sorted(networks, key=lambda x: (x['ssid'], -x['rssi']))
+    elif sort_by == 'rssi_ssid':
+        sorted_networks = sorted(networks, key=lambda x: (-x['rssi'], x['ssid']))
+    elif sort_by == 'channel':
+        sorted_networks = sorted(networks, key=lambda x: x['channel'])
+    else:
+        raise ValueError("wrong input")
+
+    return sorted_networks
+
+def process_data(data):
+    received_data = data.decode()
+    networks = []
+    for line in received_data.split('\n'):
+        if line:
+            ssid, rssi, channel, encryption = line.split('$')
+            network = {
+                'ssid': ssid,
+                'rssi': int(rssi),
+                'channel': int(channel),
+                'encryption': encryption
+            }
+            networks.append(network)
+    
+    # sort_by=ssid/rssi/ssid_rssi/rssi_ssid/channel
+    sorted_networks = sort_wifi_networks(networks, sort_by='rssi')
+
+    for network in sorted_networks:
+        print(network)
+    print("\n")
+
+def receive_data():
+    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSock.bind((UDP_IP_ADDRESS, UDP_PORT))
+    print("Listening for UDP packets on {}:{}".format(UDP_IP_ADDRESS, UDP_PORT))
+    while True:
+        data, addr = serverSock.recvfrom(1024)
+        thread = threading.Thread(target=process_data, args=(data,))
+        thread.start()
+
+if __name__ == '__main__':
+    receive_data()
